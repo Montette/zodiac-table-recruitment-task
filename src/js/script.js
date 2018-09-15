86089a7ed8d4938a4eaa90a3d993e4ee5b5b122e
Monika Radosiewicz <monika.radosiewicz@gmail.com>
2018-09-15 23:01:10 +0200

    style modal

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
 document.querySelectorAll('.fa-trash').forEach(icon => {
     icon.addEventListener('click', (event)=> {
         console.log('gg');
-        event.currentTarget.parentElement.nextElementSibling.classList.add('active');
+        // event.currentTarget.parentElement.nextElementSibling.classList.add('active');
         event.currentTarget.parentElement.parentElement.classList.add('remove')
     })
 })

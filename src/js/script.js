b1521b23944c4e872f97a1e40f4e68bf264b040f
Monika Radosiewicz <monika.radosiewicz@gmail.com>
2018-09-15 23:49:47 +0200

    add in JS file animate removing user from DOM after click in confirm modal button

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,40 @@
-document.querySelectorAll('.fa-trash').forEach(icon => {
-    icon.addEventListener('click', (event)=> {
-        console.log('gg');
-        // event.currentTarget.parentElement.nextElementSibling.classList.add('active');
-        event.currentTarget.parentElement.parentElement.classList.add('remove')
+(function (){
+
+const users = document.querySelectorAll('.user');
+
+const showModal = (user) => {
+    user.querySelector('.modal').classList.add('active');
+    user.classList.add('remove')
+}
+
+const removeFromDOM = (event) => {
+    event.target.remove();
+}
+
+const removeUser = (user) => {
+    // user.querySelector('.modal').classList.remove('active');
+    user.classList.add('remove-animation');
+    user.addEventListener('animationend', removeFromDOM)
+
+}
+
+const cancelRemoving = (user) => {
+    user.querySelector('.modal').classList.remove('active');
+    user.classList.remove('remove')
+}
+
+users.forEach(user => {
+    user.addEventListener('click', ()=> {
+        console.log(event.target)
+        if(event.target.classList.contains('action__button')) {
+            showModal(user)
+        }
+        if(event.target.classList.contains('modal__button--confirm')) {
+            removeUser(user)
+        }
+        if(event.target.classList.contains('modal__button--reject')) {
+            cancelRemoving(user)
+        }
     })
 })
+})()
